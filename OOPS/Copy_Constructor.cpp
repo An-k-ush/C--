4b7591572fbeac783e13e5b1275d6c59c86f2a33
Ankush <sounankush123@gmail.com>
2023-10-18 12:48:21 +0200

    commit

---
+++ OOPS/Copy_Constructor.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Milf {
+    char milf_cup_size;
+    int chest_size;
+    string pussy_color;
+    public:
+    void set_milf_cup(char ch) {
+        milf_cup_size = ch;
+    }
+    void set_chest_size(int n) {
+        chest_size = n;
+    }
+    void set_pussy_color(string s) {
+        pussy_color = s;
+    }
+    void print_milf_details() {
+        cout << "Cup Size --> " << milf_cup_size<<endl;
+        cout << "Chest Size -->" << chest_size <<" cm" <<endl;
+        cout << "Pussy Color -->" << pussy_color << endl;
+    }
+};
+int main() {
+    Milf Ankit;
+    Ankit.set_chest_size(106);
+    Ankit.set_milf_cup('E');
+    Ankit.set_pussy_color("Pink");
+    Ankit.print_milf_details();
+}
